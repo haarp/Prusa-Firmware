d6bf93107c94cbfefb6ef84709a857ee5c45509d
haarp <haarp@users.noreply.github.com>
2019-08-30 17:08:52 +0200

    Further loosen pulley check limits

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -7479,8 +7479,8 @@ static bool lcd_selfcheck_axis_sg(unsigned char axis) {
 
 		printf_P(_N("Axis length difference:%.3f\n"), abs(measured_axis_length[0] - measured_axis_length[1]));
 	
-#if defined(X_AXIS_MOTOR_09) || defined(Y_AXIS_MOTOR_09) || defined(Z_AXIS_MOTOR_09)
-		if (abs(measured_axis_length[0] - measured_axis_length[1]) > 2) { //loosened check
+#if defined(X_AXIS_MOTOR_09) || defined(Y_AXIS_MOTOR_09)
+		if (abs(measured_axis_length[0] - measured_axis_length[1]) > 4) { //loosened check
 #else
 		if (abs(measured_axis_length[0] - measured_axis_length[1]) > 1) { //check if difference between first and second measurement is low
 #endif
